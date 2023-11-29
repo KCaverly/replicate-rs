461bea88c7a2c17c5b9741b30f0469a8b6cf3696
KCaverly <kylebcaverly@gmail.com>
2023-11-29 06:45:01 -0500

    updated for package metadata and removed unneeded tokio time import

--- src/predictions.rs
+++ src/predictions.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 
 use erased_serde::Serialize;
+use futures_lite::io::AsyncReadExt;
 use isahc::{prelude::*, Request};
 use serde_json::Value;
 
@@ -9,19 +10,29 @@ use crate::models::Model;
 
 #[derive(serde::Deserialize, Debug)]
 pub struct Prediction {
-    id: String,
-    model: String,
-    version: String,
-    input: Value,
-    status: String,
-    created_at: String,
-    urls: PredictionUrls,
+    pub id: String,
+    pub model: String,
+    pub version: String,
+    pub input: Value,
+    pub status: PredictionStatus,
+    pub created_at: String,
+    pub urls: PredictionUrls,
+}
+
+#[serde(rename_all = "lowercase")]
+#[derive(serde::Deserialize, Debug)]
+pub enum PredictionStatus {
+    Starting,
+    Processing,
+    Succeeded,
+    Failed,
+    Canceled,
 }
 
 #[derive(serde::Deserialize, Debug)]
-struct PredictionUrls {
-    cancel: String,
-    get: String,
+pub struct PredictionUrls {
+    pub cancel: String,
+    pub get: String,
 }
 
 #[derive(serde::Serialize)]
@@ -48,17 +59,33 @@ impl Prediction {
         let body = serde_json::to_string(&input)?;
         let mut response = Request::post(endpoint)
             .header("Authorization", format!("Token {api_key}"))
-            .body(body)
-            .unwrap()
-            .send()
-            .unwrap();
+            .body(body)?
+            .send_async()
+            .await?;
 
         let mut data = String::new();
-        response.body_mut().read_to_string(&mut data)?;
+        response.body_mut().read_to_string(&mut data).await?;
 
         let prediction: Prediction = serde_json::from_str(data.as_str())?;
 
         anyhow::Ok(prediction)
+    }
+
+    pub async fn reload(&mut self) -> anyhow::Result<()> {
+        let api_key = api_key()?;
+        let mut response = Request::get(&self.urls.get)
+            .header("Authorization", format!("Token {api_key}"))
+            .body({})?
+            .send_async()
+            .await?;
+
+        let mut data = String::new();
+        response.body_mut().read_to_string(&mut data).await?;
+
+        let prediction: Prediction = serde_json::from_str(data.as_str())?;
+        *self = prediction;
+
+        anyhow::Ok(())
     }
 }
 
@@ -88,5 +115,30 @@ mod tests {
         Prediction::create_from_model_details("nateraw", "bge-large-en-v1.5", input)
             .await
             .unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_create_and_reload_prediction() {
+        #[derive(Serialize)]
+        struct EmbeddingsInput {
+            texts: String,
+            batch_size: usize,
+            normalize_embeddings: bool,
+            convert_to_numpy: bool,
+        }
+
+        let input = Box::new(EmbeddingsInput {
+            texts: r#"["In the water, fish are swimming.", "Fish swim in the water."]"#.to_string(),
+            batch_size: 32,
+            normalize_embeddings: true,
+            convert_to_numpy: false,
+        });
+
+        let mut prediction =
+            Prediction::create_from_model_details("nateraw", "bge-large-en-v1.5", input)
+                .await
+                .unwrap();
+
+        prediction.reload().await.unwrap();
     }
 }
